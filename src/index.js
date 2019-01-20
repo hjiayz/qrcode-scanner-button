c8822053022984be3b019f18d46379dce1dfce70
hjiayz <hjiayz@hotmail.com>
2019-01-20 10:09:17 +0800

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ screen.style.width = "100%";
 screen.style.height = "100%";
 screen.style.justifyContent = "center";
 screen.style.alignItems = "center";
+screen.style.overflow = "hidden";
 
 var timer;
 
@@ -25,12 +26,14 @@ mask.style.height = "100%";
 mask.style.width = "100%";
 
 let canvas = document.createElement("canvas");
-canvas.style.width = "30%";
-canvas.style.height = "30%";
 canvas.style.zIndex = 40;
 canvas.style.position = "absolute";
 canvas.style.backgroundColor = "white";
 canvas.style.border = "1px solid green";
+canvas.style.width = "300px";
+canvas.style.height = "300px";
+canvas.width = 300;
+canvas.height = 300;
 
 function close() {
     window.clearInterval(timer);
@@ -60,13 +63,10 @@ function click() {
     return new Promise((res, err) => {
         var ctx = canvas.getContext("2d");
         let computeFrame = () => {
-            if (video.videoWidth <= 0) return;
-            video.style.width = screen.clientWidth;
-            canvas.width = video.videoWidth * 0.3;
-            if (video.videoHeight <= 0) return;
-            video.style.height = screen.clientHeight;
-            canvas.height = video.videoHeight * 0.3;
-            ctx.drawImage(video, video.videoWidth * 0.35, video.videoHeight * 0.35, video.videoWidth * 0.3, video.videoHeight * 0.3, 0, 0, video.videoWidth * 0.3, video.videoHeight * 0.3);
+            if ((video.videoWidth <= 0) || (video.videoHeight <= 0)) return;
+            video.style.width = video.videoWidth + "px";
+            video.style.height = video.videoHeight + "px";
+            ctx.drawImage(video, (video.videoWidth - 300) / 2, (video.videoHeight - 300) / 2, 300, 300, 0, 0, 300, 300);
             const luminanceSource = new HTMLCanvasElementLuminanceSource(canvas);
             const binaryBitmap = new BinaryBitmap(
                 new HybridBinarizer(luminanceSource)
